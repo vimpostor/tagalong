d5d4d086c67cd3163269da623c088d8087597f89
Magnus Groß <magnus@mggross.com>
2025-06-26 23:10:53 +0200

    Add missing tag properties

--- src/barbershop/tag.hpp
+++ src/barbershop/tag.hpp
@@ -10,6 +10,23 @@ class Tag {
 	Q_GADGET
 	Q_PROPERTY(int id MEMBER id CONSTANT)
 	Q_PROPERTY(QString title MEMBER title CONSTANT)
+	Q_PROPERTY(QString altTitle MEMBER altTitle CONSTANT)
+	Q_PROPERTY(QString key MEMBER key CONSTANT)
+	Q_PROPERTY(int parts MEMBER parts CONSTANT)
+	Q_PROPERTY(QString notes MEMBER notes CONSTANT)
+	Q_PROPERTY(QString arranger MEMBER arranger CONSTANT)
+	Q_PROPERTY(QString arranged MEMBER arranged CONSTANT)
+	Q_PROPERTY(QString sungBy MEMBER sungBy CONSTANT)
+	Q_PROPERTY(QString sungYear MEMBER sungYear CONSTANT)
+	Q_PROPERTY(QString quartet MEMBER quartet CONSTANT)
+	Q_PROPERTY(QString posted MEMBER posted CONSTANT)
+	Q_PROPERTY(QString collection MEMBER collection CONSTANT)
+	Q_PROPERTY(float rating MEMBER rating CONSTANT)
+	Q_PROPERTY(int ratingCount MEMBER ratingCount CONSTANT)
+	Q_PROPERTY(int downloaded MEMBER downloaded CONSTANT)
+	Q_PROPERTY(QUrl sheetmusic MEMBER sheetmusic CONSTANT)
+	Q_PROPERTY(QUrl sheetMusicAlt MEMBER sheetMusicAlt CONSTANT)
+
 	Q_PROPERTY(QUrl sheetmusicUrl MEMBER sheetMusicAlt CONSTANT)
 	Q_PROPERTY(QUrl sheetmusiclocation MEMBER sheetmusiclocation CONSTANT)
 public:
